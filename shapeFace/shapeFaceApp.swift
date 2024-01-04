8b6eadb528f90f337b666df94165a6ff8e536f79
hahacold <taiwanhank0908@gmail.com>
2024-01-04 14:17:45 +0800

    Initial Commit

---
+++ shapeFace/shapeFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  shapeFaceApp.swift
+//  shapeFace
+//
+//  Created by test on 2024/1/4.
+//
+
+import SwiftUI
+
+@main
+struct shapeFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
